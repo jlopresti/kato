e25dc31a9b835cd6e1c71a45e458eb8168148c8b
Julien LO PRESTI <julien.lopresti@outlook.com>
2015-06-12 08:50:37 +0200

    move jenkins client api project to src folder

--- src/JenkinsApiClient/ConsoleOuput.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace JenkinsApiClient
-{
-	public class ConsoleOutput
-	{
-		public string Text { get; set; }
-		public long Offset { get; set; }
-		public bool IsBuilding { get; set; }
-	}
-}
